57f07f4a29c7a8715ce1cdb7c0010905994dde2c
visuraprabod <visuraprabod@gmail.com>
2020-11-28 09:56:59 +0530

    remove boiler plate codes from script.js

--- js/script.js
+++ js/script.js
@@ -1,81 +1,71 @@
-$('#li-dashboard').click(function (){
-    $('#manage-customer').addClass('d-none');
-    $('#manage-items').addClass('d-none');
-    $('#place-order').addClass('d-none');
-    $('#search-order').addClass('d-none');
-    $('#dashboard').removeClass('d-none');
-
-
-    $('#li-dashboard').children().addClass('active');
-    $('#li-manage-customers').children().removeClass('active');
-    $('#li-search-orders').children().removeClass('active');
-    $('#li-place-orders').children().removeClass('active');
-    $('#li-manage-items').children().removeClass('active');
-
-
+$('#li-dashboard').click(function () {
+    navigation("DASHBOARD")
 });
 
 
-$('#li-manage-customers').click(function (){
-    $('#dashboard').addClass('d-none');
-    $('#manage-items').addClass('d-none');
-    $('#place-order').addClass('d-none');
-    $('#search-order').addClass('d-none');
-    $('#manage-customer').removeClass('d-none');
-
-
-    $('#li-dashboard').children().removeClass('active');
-    $('#li-manage-items').children().removeClass('active');
-    $('#li-place-orders').children().removeClass('active');
-    $('#li-search-orders').children().removeClass('active');
-    $('#li-manage-customers').children().addClass('active');
-
+$('#li-manage-customers').click(function () {
+    navigation("MANAGE_CUSTOMERS")
 });
 
 
-$('#li-manage-items').click(function (){
-    $('#manage-customer').addClass('d-none');
-    $('#dashboard').addClass('d-none');
-    $('#place-order').addClass('d-none');
-    $('#search-order').addClass('d-none');
-    $('#manage-items').removeClass('d-none');
-
-    $('#li-dashboard').children().removeClass('active');
-    $('#li-manage-customers').children().removeClass('active');
-    $('#li-search-orders').children().removeClass('active');
-    $('#li-place-orders').children().removeClass('active');
-    $('#li-manage-items').children().addClass('active');
-
-
+$('#li-manage-items').click(function () {
+    navigation("MANGE_ITEMS")
 });
 
 
-$('#li-place-orders').click(function (){
-    $('#manage-customer').addClass('d-none');
-    $('#manage-items').addClass('d-none');
-    $('#dashboard').addClass('d-none');
-    $('#search-order').addClass('d-none');
-    $('#place-order').removeClass('d-none');
-
-    $('#li-manage-items').children().removeClass('active');
-    $('#li-manage-customers').children().removeClass('active');
-    $('#li-search-orders').children().removeClass('active');
-    $('#li-dashboard').children().removeClass('active');
-    $('#li-place-orders').children().addClass('active');
+$('#li-place-orders').click(function () {
+    navigation("PLACE_ORDER")
 });
 
 
-$('#li-search-orders').click(function (){
-    $('#manage-customer').addClass('d-none');
-    $('#manage-items').addClass('d-none');
-    $('#place-order').addClass('d-none');
-    $('#dashboard').addClass('d-none');
-    $('#search-order').removeClass('d-none');
-
-
-    $('#li-manage-items').children().removeClass('active');
-    $('#li-manage-customers').children().removeClass('active');
-    $('#li-dashboard').children().removeClass('active');
-    $('#li-place-orders').children().removeClass('active');
-    $('#li-search-orders').children().addClass('active');
+$('#li-search-orders').click(function () {
+    navigation("SEARCH_ORDER")
 });
+
+
+function navigation(pageName) {
+    $('.nav-item').children().removeClass('active');
+    $('.content-wrapper').addClass('d-none');
+    switch (pageName) {
+        case "DASHBOARD":
+
+            $('#dashboard').removeClass('d-none');
+            $('#li-dashboard').children().addClass('active');
+            break;
+        case "MANAGE_CUSTOMERS":
+
+            $('#manage-customer').removeClass('d-none');
+            $('#li-manage-customers').children().addClass('active');
+            break;
+        case "MANGE_ITEMS":
+
+            $('#manage-items').removeClass('d-none');
+            $('#li-manage-items').children().addClass('active');
+            break;
+        case "PLACE_ORDER":
+
+            $('#place-order').removeClass('d-none');
+            $('#li-place-orders').children().addClass('active');
+            break;
+        case "SEARCH_ORDER":
+
+            $('#search-order').removeClass('d-none');
+            $('#li-search-orders').children().addClass('active');
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
